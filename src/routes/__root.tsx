9c57d8dd147f85ebd34d4f06d7bf2ecc0f98a93e
cyberpotato2077 <cyberpotato96@gmail.com>
2025-07-12 02:40:46 +0900

    feat: route poc (#2)
    * pnpm
    
    * pnpm lock
    
    * h
    
    * gggg
    
    * gggasfasafdg

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,9 +1,18 @@
-import { Outlet, createRootRoute } from '@tanstack/react-router'
+import { Link, Outlet, createRootRoute } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 
 export const Route = createRootRoute({
   component: () => (
     <>
+      <div className="p-2 flex gap-2">
+        <Link to="/" className="[&.active]:font-bold">
+          Home
+        </Link>{' '}
+        <Link to="/chat" className="[&.active]:font-bold">
+          About
+        </Link>
+      </div>
+      <hr />
       <Outlet />
       <TanStackRouterDevtools />
     </>
